d229d4646fb921b76a24c3b2b800bfac2a14d536
Sean Velasco <seanvelasco@ieee.org>
2024-05-15 02:50:45 +0800

    Initial Commit

---
+++ baybayin/baybayinApp.swift
@@ -0,0 +1,17 @@
+//
+//  baybayinApp.swift
+//  baybayin
+//
+//  Created by Sean Velasco on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct baybayinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
